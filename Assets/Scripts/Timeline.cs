044d6c8d8644d2cc93bc871b0f0b54766c793263
Peter Bickhofe <peter@bickhofe.de>
2019-04-12 15:35:17 +0200

    manipulationg time

--- Assets/Scripts/Timeline.cs
+++ Assets/Scripts/Timeline.cs
@@ -7,7 +7,37 @@ public class Timeline : MonoBehaviour {
 
 	public PlayableDirector spielberg;
 
+	public bool bulletTime = false;
+
 	public void Play () {
+		spielberg.Play();
+	}
+
+	public void Stop () {
+		spielberg.time = 0;
+		spielberg.Evaluate();
+		spielberg.Stop();
+	}
+
+	public void Pause () {
+		spielberg.Pause();
+	}
+
+	public void Slow () {
+		spielberg.timeUpdateMode = DirectorUpdateMode.Manual;
+		spielberg.time += Time.deltaTime/10;
+		spielberg.Evaluate();
+	}
+
+	public void Fast () {
+		spielberg.timeUpdateMode = DirectorUpdateMode.Manual;
+		spielberg.time += Time.deltaTime*2;
+		spielberg.Evaluate();
+	}
+
+	public void Normal () {
+		spielberg.Pause();
+		spielberg.timeUpdateMode = DirectorUpdateMode.GameTime;
 		spielberg.Play();
 	}
 
@@ -15,6 +45,25 @@ public class Timeline : MonoBehaviour {
 		if (Input.GetMouseButtonDown(0)){
 			Play ();
 		}
+
+		if (Input.GetMouseButtonDown(1)){
+			Stop ();
+		}
+
+		// if (Input.GetMouseButtonDown(2)){
+		// 	Pause ();
+		// }
+
+		if (Input.GetMouseButtonDown(2)){
+			bulletTime = true;
+		}
+
+		if (Input.GetMouseButtonUp(2)){
+			bulletTime = false;
+			Normal ();
+		}
+
+		if (bulletTime) Slow ();
 	}
 	
 
