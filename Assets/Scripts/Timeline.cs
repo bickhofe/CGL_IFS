0b8f080441d72b96ff9cad238b8855ec29415c8d
Peter Bickhofe <peter@bickhofe.de>
2019-04-12 14:57:14 +0200

    timeline demo

---
+++ Assets/Scripts/Timeline.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class Timeline : MonoBehaviour {
+
+	public PlayableDirector spielberg;
+
+	public void Play () {
+		spielberg.Play();
+	}
+
+	void Update(){
+		if (Input.GetMouseButtonDown(0)){
+			Play ();
+		}
+	}
+	
+
+}
